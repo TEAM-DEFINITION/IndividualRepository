71856aa60c9d3b086bcfa7881d929043f839f6c1
wndnjs0249@naver.com <wndnjs0249@naver.com>
2021-05-06 18:29:28 +0900

    bottom_navigation_bar_kimjuwon

---
+++ 20210505/kimjuwon/hybrid_access_app/lib/root_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hybrid_access_app/login_page.dart';
+import 'tab_page.dart';
+import 'login_page.dart';
+
+class RootPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LoginPage();
+  }
+}
